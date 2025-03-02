f8065d885850efdab7769a12f0d04c452bc9d4ae
fxhxyz <fxhxyz7@proton.me>
2025-03-02 19:34:07 +0000

    fetcher

---
+++ src/modules/utils/promiseFetcher.js
@@ -0,0 +1,15 @@
+import { Messages } from "./messages.js";
+import axios from "axios";
+
+const promiseFetcher = async (Url) => {
+  if (!Url) {
+    Messages.error(`Url: ${Url} invalid`);
+  }
+
+  const response = await axios.get(Url);
+  const result = response.data;
+
+  return result;
+}
+
+export { promiseFetcher };
