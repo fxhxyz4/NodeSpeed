60d2edc75c33c0c7b92fec33bf2b36e57d6b88a4
fxhxyz <fxhxyz7@proton.me>
2025-03-02 19:29:38 +0000

    contact

---
+++ src/modules/cmd/contactCmd.js
@@ -0,0 +1,17 @@
+import { PackageJSON } from "../utils/packageJson.js";
+import { Messages } from "../utils/messages.js";
+
+const { AUTHOR_NAME, AUTHOR_EMAIL, AUTHOR_URL } = PackageJSON;
+
+const contactCmd = () => {
+    Messages.log("\n");
+    Messages.log(`author: ${AUTHOR_NAME}`);
+
+    Messages.log(`email: ${AUTHOR_EMAIL}`);
+    Messages.log(`website: ${AUTHOR_URL.replace("https://", "")}`);
+
+    Messages.log("\n");
+    process.exit(0);
+}
+
+export { contactCmd };
