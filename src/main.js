209ca12751f5e1cf4e70578f0a1797c07a756730
fxhxyz <fxhxyz7@proton.me>
2025-03-02 19:28:34 +0000

    main

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import { showAscii } from './modules/utils/showAscii.js';
+import { parseArgs } from './modules/utils/parseArgs.js';
+import { cli } from './modules/utils/cli.js';
+
+/*
+ * @start program
+ */
+const main = () => {
+	const ARGV = process.argv.slice(2);
+	const PARSE = parseArgs(ARGV);
+
+	showAscii();
+	cli(PARSE);
+};
+
+main();
