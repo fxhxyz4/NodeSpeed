ad82a7277ddcb745110609d2bde3fc1e8907a42b
fxhxyz <fxhxyz7@proton.me>
2025-03-02 19:31:01 +0000

    versionCmd

---
+++ src/modules/cmd/versionCmd.js
@@ -0,0 +1,11 @@
+import { PackageJSON } from "../utils/packageJson.js";
+import { Messages } from "../utils/messages.js";
+
+const versionCmd = () => {
+    Messages.log(`\x1b[94mNodeSpeed version: \x1b[31m${PackageJSON.VERSION}`);
+    Messages.log("\n\n");
+
+    process.exit(0);
+}
+
+export { versionCmd };
