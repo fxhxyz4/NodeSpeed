90b45e07feeaa9a76d388f8393a5a9e4cfe627fc
fxhxyz <fxhxyz7@proton.me>
2025-03-02 19:33:40 +0000

    argv parser

---
+++ src/modules/utils/parseArgs.js
@@ -0,0 +1,12 @@
+const parseArgs = (Arguments) => {
+    let parse = Object.fromEntries(
+        Arguments.map(arg => {
+            let [key, value] = arg.split("=");
+            return [key.replace(/^--?/, ''), value];
+        })
+    )
+
+    return parse;
+}
+
+export { parseArgs };
