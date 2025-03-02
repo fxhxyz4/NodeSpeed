b9a8f1be512052b1173094307d154165cdbea1dd
fxhxyz <fxhxyz7@proton.me>
2025-03-02 19:34:25 +0000

    _

---
+++ src/modules/utils/searchIncorrectWords.js
@@ -0,0 +1,32 @@
+const searchIncorrectWords = (Answer, Source) => {
+    const arrayAnswer = Answer.split(/\s+/).filter(word => word.trim() !== "");
+    const arraySource = Source.split(/\s+/).filter(word => word.trim() !== "");
+
+    let repeatString = "";
+
+    if (arrayAnswer.length !== arraySource.length) {
+        const lengthDifference = Math.abs(arrayAnswer.length - arraySource.length);
+
+        if (arrayAnswer.length < arraySource.length) {
+            for (let i = 0; i < lengthDifference; i++) {
+                arrayAnswer.push(repeatString);
+            }
+        } else {
+            for (let i = 0; i < lengthDifference; i++) {
+                arraySource.push(repeatString);
+            }
+        }
+    }
+
+    let incorrectCounter = 0;
+
+    arrayAnswer.forEach((word, index) => {
+        if (arraySource[index] !== word) {
+            incorrectCounter++;
+        }
+    });
+
+    return incorrectCounter;
+}
+
+export { searchIncorrectWords };
